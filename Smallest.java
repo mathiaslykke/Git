75a093c1f9fdba58a34c5d7ca78ffd381fe8f606
Mathias Lykke Jensen <mathiaslykkejensen@Mathiass-MacBook-Pro.local>
2019-10-31 15:13:17 +0100

    nu

--- Smallest.java
+++ Smallest.java
@@ -4,7 +4,7 @@ import java.util.*;
  * Smallest
  */
 public class Smallest {
-
+// documentation
     public static void main(String[] args) {
         
         int smallest = new Random()
@@ -19,6 +19,11 @@ public class Smallest {
     var f = 20;
     var ce = 25;
     var fefe =2;
+    var ef = 1;
+    var tre = 1;
+
+    addinter();
+    var eeeee = 1;
 
     }
 }
